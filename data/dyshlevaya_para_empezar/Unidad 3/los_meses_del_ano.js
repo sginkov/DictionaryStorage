4728169c1609928d0e02822dc3b24af9aa688051
s.ginkov <s.ginkov@gmail.com>
2023-09-01 12:40:51 +0200

    added 5 unidades and changed format

---
+++ data/dyshlevaya_para_empezar/Unidad 3/los_meses_del_ano.js
@@ -0,0 +1,18 @@
+[
+    {"es":"el invierno", "ru":"зима"},
+    {"es":"la primavera", "ru":"весна"},
+    {"es":"el verano", "ru":"лето"},
+    {"es":"el otoño", "ru":"осень"},
+    {"es":"enero", "ru":"январь"},
+    {"es":"febrero", "ru":"фераль"},
+    {"es":"marzo", "ru":"март"},
+    {"es":"abril", "ru":"апрель"},
+    {"es":"mayo", "ru":"май"},
+    {"es":"junio", "ru":"июнь"},
+    {"es":"julio", "ru":"июль"},
+    {"es":"agosto", "ru":"август"},
+    {"es":"septiembre", "ru":"сентябрь"},
+    {"es":"octubre", "ru":"октябрь"},
+    {"es":"noviembre", "ru":"ноябрь"},
+    {"es":"diciembre", "ru":"декабрь"},
+]
